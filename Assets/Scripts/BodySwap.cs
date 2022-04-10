40cfe998a615b7e40c0d78466367a20989e376d7
Eli-PC <rxelik5@gmail.com>
2022-04-10 18:14:22 +0300

    working pooler

--- Assets/Scripts/BodySwap.cs
+++ Assets/Scripts/BodySwap.cs
@@ -7,7 +7,12 @@ public class BodySwap : MonoBehaviour
     public GameObject player;
     RougeController rougeController;
     GameObject _camera;
-    
+
+    private void OnEnable()
+    {
+        //gameObject.transform.rotation = Quaternion.Euler(Vector3.zero);
+        //gameObject.transform.position = Vector3.zero;
+    }
     private void Start()
     {
         player = GameObject.Find("Player 2D");
@@ -20,12 +25,11 @@ public class BodySwap : MonoBehaviour
             player.GetComponent<RougeController>().enabled = false;
             collision.gameObject.GetComponent<RougeController>().enabled = true;
             _camera.transform.SetParent(collision.transform, false);
-<<<<<<< HEAD
             Destroy(gameObject);
-=======
+
             //Destroy(gameObject);
             gameObject.SetActive(false);
->>>>>>> d304b0d0d893b133da15b939214a8313c9f9996f
+
             GameManager.instance.currentPlayer.Add(collision.gameObject);
         }
 
@@ -34,12 +38,12 @@ public class BodySwap : MonoBehaviour
             GameManager.instance.currentPlayer[0].GetComponent<RougeController>().enabled = false;
             player.GetComponent<RougeController>().enabled = true;
             _camera.transform.SetParent(collision.transform, false);
-<<<<<<< HEAD
+
             Destroy(gameObject);
-=======
+
             //Destroy(gameObject);
             gameObject.SetActive(false);
->>>>>>> d304b0d0d893b133da15b939214a8313c9f9996f
+
         }
     }
 }
--- Assets/Scripts/RougeController.cs
+++ Assets/Scripts/RougeController.cs
@@ -21,10 +21,7 @@ public class RougeController : MonoBehaviour
     public float moveSpeed;
     public int jumpSpeed;
 
-<<<<<<< HEAD
-=======
     Pooler objPooler;
->>>>>>> d304b0d0d893b133da15b939214a8313c9f9996f
     public GameObject bullet;
     public GameObject currentBall;
     public Transform bulletResPos;
@@ -44,10 +41,8 @@ public class RougeController : MonoBehaviour
     private void Start()
     {
         // Lock rotation of rb - 2.5D Effect
-<<<<<<< HEAD
-=======
         objPooler = Pooler.Instance;
->>>>>>> d304b0d0d893b133da15b939214a8313c9f9996f
+
         rb = GetComponent<Rigidbody>();
         rb.centerOfMass = Vector3.zero;
         rb.inertiaTensorRotation = Quaternion.identity;
@@ -63,15 +58,13 @@ public class RougeController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.E))
         {
-<<<<<<< HEAD
-            currentBall = Instantiate(bullet, bulletResPos.position, bulletResPos.rotation);
-=======
-            //Switched to pooling
-            objPooler.SpawnFromPool("Hat", bulletResPos.position, bulletResPos.rotation);
-            //currentBall = Instantiate(bullet, bulletResPos.position, bulletResPos.rotation);
->>>>>>> d304b0d0d893b133da15b939214a8313c9f9996f
-            currentBall.GetComponent<Rigidbody>().AddRelativeForce(new Vector3
-                                                     (bulletVelocity, 0, 0));
+            currentBall = objPooler.SpawnFromPool("Hat", bulletResPos.position, bulletResPos.rotation);
+
+            if (sprite.transform.rotation == new Quaternion(0, 180, 0, 0))
+                currentBall.GetComponent<Rigidbody>().velocity = Vector3.right * bulletVelocity;
+            else
+                currentBall.GetComponent<Rigidbody>().velocity = Vector3.left * bulletVelocity;
+
         }
 
         if (Input.GetKeyDown(KeyCode.A))
