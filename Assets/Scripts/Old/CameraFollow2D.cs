27b476f701d27614bfc28b673396a01203c0f058
RoxasCodes <evicokazzz@gmail.com>
2022-05-24 18:47:45 +0300

    camera and crouch

--- Assets/Scripts/Old/CameraFollow2D.cs
+++ Assets/Scripts/Old/CameraFollow2D.cs
@@ -6,8 +6,11 @@ public class CameraFollow2D : MonoBehaviour
 {
     public Transform target;
     public Transform crouchPos;
-    public float smoothSpeed = 0.125f;
+    public bool IsCrouch = false;
+    public float smoothSpeed = 0.25f;
     public Vector3 offset;
+    private Vector3 velocity = Vector3.zero;
+
 
     [HideInInspector] public Vector3 frontView;
     [HideInInspector] public Vector3 sideView;
@@ -16,30 +19,36 @@ public class CameraFollow2D : MonoBehaviour
 
     private void Start()
     {
-        frontView = new Vector3(0, offset.y, offset.z);
-        sideView = new Vector3(90, offset.y, offset.z);
+        //frontView = new Vector3(0, offset.y, offset.z);
+        //sideView = new Vector3(90, offset.y, offset.z);
     }
     private void Update()
     {
+        FollowAndCrouch();
         //currentY = new Vector3(offset.x, offset.y, gameObject.transform.position.z);
         //currentZ = new Vector3(offset.x, gameObject.transform.position.y, offset.z);
-        Crouch();
     }
     void FixedUpdate()
     {
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = smoothedPosition;
+        //Vector3 desiredPosition = target.position + offset;
+        //Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        //transform.position = smoothedPosition;
 
-        transform.LookAt(target);
+        //transform.LookAt(target);
         //target.LookAt(gameObject.transform);
     }
 
-    void Crouch()
+    void FollowAndCrouch()
     {
         if (Input.GetKey(KeyCode.DownArrow))
         {
-
+            Vector3 targetCamPosition = crouchPos.position;
+            transform.position = Vector3.SmoothDamp(transform.position, targetCamPosition, ref velocity, smoothSpeed);
+        }
+        else
+        {
+            Vector3 targetPosition = target.position + offset;
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothSpeed);
         }
     }
 }
