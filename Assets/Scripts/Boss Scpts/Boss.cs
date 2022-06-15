8194c2aa48188d87d4cfad4b4f63020bc7367d49
Eli-PC <rxelik5@gmail.com>
2022-06-15 13:48:50 +0300

    MaybeBoss and start of flying book

---
+++ Assets/Scripts/Boss Scpts/Boss.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    public int _hp;
+    public GameObject Hands;
+    public ParticleSystem Dmg;
+    public Animation attack;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
